7f0eccca8ea68cb0976bd140261b6d24654bb9ae
SNOWNUMB <mamayev.stas@gmail.com>
2026-02-28 22:11:18 +0500

    feat: добавлены валидации для нового пароля в запросе на сброс пароля администратором

--- backend/src/main/java/com/stackscout/dto/AdminResetPasswordRequest.java
+++ backend/src/main/java/com/stackscout/dto/AdminResetPasswordRequest.java
@@ -5,10 +5,19 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+/**
+ * Запрос на сброс пароля пользователя администратором
+ */
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class AdminResetPasswordRequest {
+    
+    @NotBlank(message = "Новый пароль обязателен")
+    @Size(min = 8, message = "Пароль должен содержать минимум 8 символов")
     private String newPassword;
 }
